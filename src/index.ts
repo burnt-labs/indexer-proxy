2ba5479c5d59cb769846dc14c2fe6e9d0f19a0fc
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-03-27 13:17:39 -0400

    updated juno testnet chain ID

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ type Env = {
 
 const INDEXER_HOST_MAP = {
   'juno-1': 'juno-mainnet.indexer.zone',
-  'uni-6': 'juno-testnet.indexer.zone',
+  'uni-7': 'juno-testnet.indexer.zone',
   'osmosis-1': 'osmosis-mainnet.indexer.zone',
   'osmo-test-5': 'osmosis-testnet.indexer.zone',
   'stargaze-1': 'stargaze-mainnet.indexer.zone',
